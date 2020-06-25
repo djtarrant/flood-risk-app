f56322ececb6aaf5b004aab471e7833e7eb970d8
Darren Tarrant <in@djtarrant.co.uk>
2020-06-25 20:51:38 +0100

    Initial React app

---
+++ react-flood-risk/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from "./components/Form";
+
+class App extends Component {
+  state = {
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Form></Form>
+      </div>
+    );
+  }
+}
+
+export default App;
